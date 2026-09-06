3a6f0cf8519963c76d7968693e50c02d3b5648eb
Salma Abdelmoaty <salmaabdelmoaty5@gmail.com>
2026-09-06 19:29:37 +0300

    Add files via upload

---
+++ headers/interrupt.h
@@ -0,0 +1,21 @@
+#ifndef _INTERRUPT_H_
+#define _INTERRUPT_H_
+
+#include "Std_types.h"
+#include <avr/io.h>
+#include "Bit_Math.h"
+#include <avr/interrupt.h>
+#include "interrupt.h"
+
+
+
+
+
+void initExternalInterupts(void);
+
+
+
+
+
+
+#endif
