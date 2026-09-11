56d7008bdcf8f3177bbdf79c2925a80e9d546247
Salma Abdelmoaty <salmaabdelmoaty5@gmail.com>
2026-09-11 17:04:20 +0300

    spi.c

--- source/spi.c
+++ source/spi.c
@@ -1,9 +1,3 @@
-/*
- * SPI.c
- *
- * Created on: Sep 9, 2026
- * Author: Hager Adel
- */
 
 #include "Std_types.h"
 #include "BIT_MATH.h"
@@ -13,36 +7,36 @@
 void SPI_Init(void)
 {
 #if SPI_MODE == SPI_MASTER
-    DIO_voidSetPinDirection(PORTB, 4, OUTPUT);
-    DIO_voidSetPinDirection(PORTB, 5, OUTPUT);
-    DIO_voidSetPinDirection(PORTB, 7, OUTPUT);
-    DIO_voidSetPinDirection(PORTB, 6, INPUT);
+    DIO_voidSetPinDirection(PORTB, PIN4, OUTPUT);
+    DIO_voidSetPinDirection(PORTB, PIN5, OUTPUT);
+    DIO_voidSetPinDirection(PORTB, PIN7, OUTPUT);
+    DIO_voidSetPinDirection(PORTB, PIN6, INPUT);
     
 
-    DIO_voidSetPinValue(PORTB, 6, HIGH);
+    DIO_voidSetPinValue(PORTB, PIN6, HIGH);
     SPCR = 0b01011010;
 
 #elif SPI_MODE == SPI_SLAVE
 
-    DIO_voidSetPinDirection(PORTB, 4, INPUT);
-    DIO_voidSetPinDirection(PORTB, 5, INPUT);
-    DIO_voidSetPinDirection(PORTB, 7, INPUT);
-    DIO_voidSetPinDirection(PORTB, 6, OUTPUT);
+    DIO_voidSetPinDirection(PORTB, PIN4, INPUT);
+    DIO_voidSetPinDirection(PORTB, PIN5, INPUT);
+    DIO_voidSetPinDirection(PORTB, PIN7, INPUT);
+    DIO_voidSetPinDirection(PORTB, PIN6, OUTPUT);
     SPCR = 0b01001010;
 #endif
 
-    CLR_BIT(SPSR, 0);
+    CLR_BIT(SPSR, PIN0);
 }
 
 void SPI_SendChar(u8 copy_u8Data)
 {
     SPDR = copy_u8Data;
-    while (!GET_BIT(SPSR, 7));
+    while (!GET_BIT(SPSR, PIN7));
 }
 
 u8 SPI_GetChar(void)
 {
-    while (!GET_BIT(SPSR, 7));
+    while (!GET_BIT(SPSR, PIN7));
     return SPDR;
 }
 
